8b078d4b0f70d0b2a06164e6db7241eaa654b0ec
Piiu Pilt <piu@piu.ee>
2019-12-22 11:32:48 +0200

    Overflow imminent

--- day22.php
+++ day22.php
@@ -1,55 +1,66 @@
 <?php
 
 $input = file_get_contents(__DIR__ . '/input/day22');
-$instructions = explode("\n", $input);
-
-$deck = new Deck();
-
-foreach ($instructions as $instruction) {
+$instructions = array_map(function (string $instruction) : array {
     preg_match('/([\w\s]*) ([-\d]*)?/', $instruction, $matches);
-    $operation = $matches[1];
-    $parameter = (int)$matches[2];
-    switch ($operation) {
-        case 'deal with increment':
-            $deck->dealWithIncrement($parameter);
-            break;
-        case 'cut':
-            $deck->cut($parameter);
-            break;
-        case 'deal into new':
-            $deck->dealIntoNewStack();
-    }
-}
+    return [
+        'operation' => $matches[1],
+        'parameter' => (int)$matches[2]
+    ];
+}, explode("\n", $input));
 
+$deck = new Deck(10007);
+$deck->shuffle($instructions);
 echo 'Part 1: '. $deck->findCard(2019) . PHP_EOL;
+
+$deck = new Deck(119315717514047);
+for ($count = 0; $count < 101741582076661; $count++) {
+    $deck->shuffle($instructions);
+}
+echo 'Part 2: '. $deck->findCard(2020) . PHP_EOL;
 
 class Deck {
     private $cards = [];
+    private $deckSize;
 
-    const DECK_SIZE = 10007;
-
-    public function __construct() {
-        $this->cards = range(0, self::DECK_SIZE - 1);
+    public function __construct(int $size) {
+        $this->deckSize = $size;
+        $this->cards = range(0, $size - 1);
     }
 
-    public function dealIntoNewStack() {
+    public function shuffle($instructions) {
+        foreach ($instructions as $instruction) {
+            switch ($instruction['operation']) {
+                case 'deal with increment':
+                    $this->dealWithIncrement($instruction['parameter']);
+                    break;
+                case 'cut':
+                    $this->cut($instruction['parameter']);
+                    break;
+                case 'deal into new':
+                    $this->dealIntoNewStack();
+            }
+        }
+    }
+
+    private function dealIntoNewStack() {
         $this->cards = array_reverse($this->cards);
     }
 
-    public function cut(int $n) {
+    private function cut(int $n) {
         $firstSlice = array_slice($this->cards, $n);
         $secondSlice = array_slice($this->cards, 0, $n);
         $this->cards = array_merge($firstSlice, $secondSlice);
     }
 
-    public function dealWithIncrement(int $n) {
+    private function dealWithIncrement(int $n) {
         $newDeck = [];
         $position = 0;
         foreach ($this->cards as $card) {
             $newDeck[$position] = $card;
             $position += $n;
-            if ($position > self::DECK_SIZE) {
-                $position -= self::DECK_SIZE;
+            if ($position > $this->deckSize) {
+                $position -= $this->deckSize;
             }
         }
         ksort($newDeck);
